a1f4e564a489a73731aee16667d2af54de064aac
MustBeAltF4 <90697465+MustBeAltF4@users.noreply.github.com>
2023-07-07 23:44:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-/*
-See on github: https://github.com/muhammederdem/credit-card-form
-*/
-
 new Vue({
   el: "#app",
   data() {
     return {
-      currentCardBackground: Math.floor(Math.random()* 25 + 1), // just for fun :D
+      currentCardBackground: Math.floor(Math.random()* 25 + 1),
       cardName: "",
       cardNumber: "",
       cardMonth: "",
       cardYear: "",
       cardCvv: "",
       minCardYear: new Date().getFullYear(),
-      amexCardMask: "#### ###### #####",
       otherCardMask: "#### #### #### ####",
-      cardNumberTemp: "",
+      cardNumberTemp: "", 
       isCardFlipped: false,
       focusElementStyle: null,
       isInputFocused: false
@@ -26,45 +21,39 @@ new Vue({
     document.getElementById("cardNumber").focus();
   },
   computed: {
-    getCardType () {
+    getCardType() {
       let number = this.cardNumber;
       let re = new RegExp("^4");
       if (number.match(re) != null) return "visa";
 
-      re = new RegExp("^(34|37)");
-      if (number.match(re) != null) return "amex";
-
       re = new RegExp("^5[1-5]");
       if (number.match(re) != null) return "mastercard";
 
-      re = new RegExp("^6011");
-      if (number.match(re) != null) return "discover";
-      
-      re = new RegExp('^9792')
-      if (number.match(re) != null) return 'troy'
+      re = new RegExp("^5[1-5]");
+      if (number.match(re) != null) return "mir";
 
-      return "visa"; // default type
+      return "mir"; // default type
     },
-		generateCardNumberMask () {
-			return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+    generateCardNumberMask() {
+      return this.otherCardMask;
     },
-    minCardMonth () {
+    minCardMonth() {
       if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
       return 1;
     }
   },
   watch: {
-    cardYear () {
+    cardYear() {
       if (this.cardMonth < this.minCardMonth) {
         this.cardMonth = "";
       }
     }
   },
   methods: {
-    flipCard (status) {
+    flipCard(status) {
       this.isCardFlipped = status;
     },
-    focusInput (e) {
+    focusInput(e) {
       this.isInputFocused = true;
       let targetRef = e.target.dataset.ref;
       let target = this.$refs[targetRef];
@@ -72,7 +61,7 @@ new Vue({
         width: `${target.offsetWidth}px`,
         height: `${target.offsetHeight}px`,
         transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
-      }
+      };
     },
     blurInput() {
       let vm = this;
